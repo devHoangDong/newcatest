8b130302b0251ffdaaa0c75319a4ac948e85e4b8
devHoangDong <60665278+devHoangDong@users.noreply.github.com>
2021-06-01 23:34:32 +0700

    update key

--- src/GoogleAuthen/index.jsx
+++ src/GoogleAuthen/index.jsx
@@ -4,7 +4,7 @@ import { useGoogleLogin } from 'react-use-googlelogin'
 const GoogleAuthContext = React.createContext()
 
 export const GoogleAuthProvider = ({ children }) => {
-    const GOOGLE_CLIENT_ID = '117400138061-jjrft3golf8phm2meimi341nck47ta5v.apps.googleusercontent.com'
+    const GOOGLE_CLIENT_ID = '117400138061-odmb892plvium4fp9uabd5255bhm0ntg.apps.googleusercontent.com'
   const googleAuth = useGoogleLogin({
     clientId: GOOGLE_CLIENT_ID, // Your clientID from Google.
   })
