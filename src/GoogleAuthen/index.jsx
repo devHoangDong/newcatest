1f46e2e0d509d0409baa223a7ff5336651cd6899
Hoàng Văn Đông <donghv@cyberlotus.com>
2021-06-08 21:32:27 +0700

    up

--- src/GoogleAuthen/index.jsx
+++ src/GoogleAuthen/index.jsx
@@ -5,8 +5,8 @@ import { useGoogleLogin } from 'react-use-googlelogin';
 const GoogleAuthContext = React.createContext()
 
 export const GoogleAuthProvider = ({ children }) => {
-  // const GOOGLE_CLIENT_ID = '117400138061-qmmhcm296ol3tqa2tc0eq60a5j553c27.apps.googleusercontent.com'
-  const GOOGLE_CLIENT_ID = '818031211174-5j50gsf9knjqec9j0lcgo05v34irl1ni.apps.googleusercontent.com'
+  // const GOOGLE_CLIENT_ID = '117400138061-qmmhcm296ol3tqa2tc0eq60a5j553c27.apps.googleusercontent.com'  //localhost
+  const GOOGLE_CLIENT_ID = '117400138061-jjrft3golf8phm2meimi341nck47ta5v.apps.googleusercontent.com'  //netflify
   const googleAuth = useGoogleLogin({
     clientId: GOOGLE_CLIENT_ID, // Your clientID from Google.
     isSignedIn: true
